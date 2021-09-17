c943a4a8f8b024ed5ba81cae4642c31b77c77599
daiivasq <daihianav@gmail.com>
2021-09-16 20:22:45 -0400

    Initial Commit

---
+++ VowelsCounterApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VowelsCounterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
